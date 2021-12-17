c38ec2e4e5633ce4e563f994441f0005aae8761f
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-12-17 13:43:09 +0200

    start solve 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
 
+btnStart.addEventListener("click", onBtnStart);
+btnStop.addEventListener("click", onBtnStop);
+
+// btnStart.addEventListener("click", () => {
+//   timerId = setInterval(() => {
+//     document.body.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+// });
+
+let timerId = null;
+
+function onBtnStart() {
+   timerId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+   }, 1000);
+}
+
+
+function onBtnStop() {
+   clearInterval(timerId);
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
