c49eebc64ddf75792972f8670a7b3d0d06acc8c5
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-12-19 22:23:02 +0200

    Счетчик запущен)

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,19 +5,36 @@ import 'flatpickr/dist/flatpickr.min.css';
 const refs = {
 input: document.querySelector("#datetime-picker"),
 btnStart: document.querySelector('button[data-start]'),
+seconds: document.querySelector('span[data-seconds]'),
+minutes: document.querySelector('span[data-minutes]'),
+hours: document.querySelector('span[data-hours]'),
+days: document.querySelector('span[data-days]'),
 }
+let futureTime;
 
 refs.btnStart.disabled = true;
 refs.btnStart.addEventListener('click', onBtnStart);
 refs.input.addEventListener('input', setDate)
 
 
-function onBtnStart(e){
-	console.log('qqq');
+function setDate(e) {
+	  futureTime = new Date(e.target.value).getTime();
 }
 
 
-function setDate(e) {
+function onBtnStart(e){
+
+setInterval(() => {
+const currentTime = Date.now();
+
+const {days, hours, minutes, seconds} = convertMs(futureTime - currentTime);
+
+  refs.seconds.textContent = addLeadingZero(seconds);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.days.textContent = addLeadingZero(days);
+  
+  }, 1000);
 
 }
 
@@ -28,22 +45,35 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-
    if(selectedDates[0].getTime() <= Date.now()){
-
 		window.alert("Please choose a date in the future");
 		} else {
 			refs.btnStart.disabled = false;
 		}
   },
 };
-
 flatpickr("#datetime-picker", options);
 
 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
 
-// class Timer {
-// 	constructor{
-		
-// 	}
-// }
+
+function addLeadingZero(value){
+	return String(value).padStart(2, '0');
+}
+
