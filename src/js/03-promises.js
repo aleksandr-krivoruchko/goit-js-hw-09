b4996c0818b3eda11a806b3a0de913b638a1269c
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-12-22 21:52:58 +0200

    Добавил notiflix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,4 @@
+import Notiflix from 'notiflix';
 
 const refs = {
   form: document.querySelector('.form'),
@@ -29,9 +30,10 @@ function onBtnSubmitCreatePromise(e) {
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+	  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+	  	  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+
   }
 }
 // createPromise(2, 1500)
