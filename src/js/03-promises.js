a20c1ff2d5b26dd07d41e125b9c617b1d3c277a2
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-12-22 13:38:42 +0200

    Update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+const refs = {
+  form: document.querySelector('.form'),
+  btnSubmit: document.querySelector('button'),
 }
+
+
+refs.form.addEventListener('input', input);
+refs.btnSubmit.addEventListener('click', onBtnSubmitCreatePromise);
+
+function input(e) {
+  e.preventDefault();
+  console.log(e.target.);
+}
+
+
+function onBtnSubmitCreatePromise(e) {
+  e.preventDefault();
+  console.log(e.target);
+}
+
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
